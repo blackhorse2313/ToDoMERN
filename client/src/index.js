0eaf3f0f3c80c0718cb3e24974fb1e2998e8bbff
kanenorth <kanenorth48@gmail.com>
2023-01-28 14:33:44 -0500

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below.
+serviceWorker.register();
